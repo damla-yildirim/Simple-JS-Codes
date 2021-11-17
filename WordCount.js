776d8309ff42facb4d072570d37f43dd26950511
Damla Yıldırım <damlayldrm4@gmail.com>
2021-11-17 22:02:47 +0300

    Create WordCount.js
    17.11.2021

---
+++ WordCount.js
@@ -0,0 +1,19 @@
+var str="Hello Earthling!"
+function WordCount(str) { 
+
+  var space=1;
+  
+    if(str=="")
+    {
+      space=0;
+    }
+    for(var i=0;i<str.length;i++)
+    {
+      if(str[i]==" ")
+      {
+        space++;
+      }
+    }  
+    console.log(space); 
+  
+  }
