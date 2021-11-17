17b28dc50d01465bf6218a322b832245c479dce8
Damla Yıldırım <damlayldrm4@gmail.com>
2021-11-17 22:48:45 +0300

    Create AlphabetSoup.js
    Alphabet Soup, alphabetical order of the words with Array.
    Output: String
    17.11.2021

---
+++ Codes/AlphabetSoup.js
@@ -0,0 +1,18 @@
+var str="challange"; //aaecghlln - string expression
+
+function AlphabetSoup(str){
+   
+        var word=Array.from(str);
+        var soup=word.sort();
+        var order=soup.toString();
+        var output="";
+
+        for(var i=0;i<order.length;i++)
+        {
+            if(order[i]!=",")
+            {
+                output=output+order[i];
+            }
+        }
+    console.log(output);
+}
